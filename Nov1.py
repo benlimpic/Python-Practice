aee4fc372cf8746e02157bd7336ad269e046d33b
init.defaultBranch <benlimpic@gmail.com>
2022-11-01 00:30:52 -0700

    late night study session of absolute basics in python

---
+++ Nov1.py
@@ -0,0 +1,121 @@
+# ---------------------------------------------------
+# # Variables
+# ---------------------------------------------------
+
+# int_value = 4
+# print(int_value)
+# print(type(int_value))
+
+# print()
+
+# float_value = float(int_value)
+# print(float_value)
+# print(type(float_value))
+
+# ---------------------------------------------------
+# # Data Types
+# ---------------------------------------------------
+
+# temperature = int(101.2)
+# print(temperature)
+# print(type(temperature))
+
+# temperature = float(101.2)
+# print(temperature)
+# print(type(temperature))
+
+# temperature = bool(101.2)
+# print(temperature)
+# print(type(temperature))
+
+# temperature = str(101.2)
+# print(temperature)
+# print(str(101.2))
+# print(type(temperature))
+
+# ---------------------------------------------------
+# # Arithmetic Operators
+# ---------------------------------------------------
+
+# x = 5
+# y = 2
+
+# print('arithmetic operator (x + y): ', x + y)
+# print('comparison operator (x >= y): ', x >= y)
+# print('logical operator (x == y and x > y): ', x == y and x > y)
+
+# print(x**y)
+# print(y**x)
+# print((x**x)*(8**4))
+
+# ---------------------------------------------------
+# # if, elif, else
+# ---------------------------------------------------
+
+# score = 91
+
+# if score >= 92:
+#    print('Your final grade is an A')
+
+# elif score >= 85:
+#    print('Your final grade is a B')
+
+# elif score >= 70:
+#    print('Your final grade is a C')
+
+# else:
+#    print('Talk with your instructor about your grade!')
+
+# ---------------------------------------------------
+# # Loops
+# ---------------------------------------------------
+
+# for loop
+# nums = [3, 4, 16]
+
+# print('This is an example of for loops')
+
+# for num in nums:
+#    print(num ** 2)
+
+# while loop
+# i = 3
+
+# print('This is an example of while loops')
+
+# while i < 258:
+#    print(i)
+#    i = i ** 2
+
+
+# ---------------------------------------------------
+# # Pass, Break, Continue
+# ---------------------------------------------------
+
+# names = ['Amanda', 'Mercedes', 'Rachel', 'Elisabeth', 'Tay', 'Xavier', 'Joaquin', 'Sam']
+
+# for name in names:
+#   if 'm' in name.lower():
+#       continue
+#   elif 'r' in name.lower():
+#       pass
+#   elif 'j' in name.lower():
+#       break
+#   else:
+#       print(name)
+
+# ---------------------------------------------------
+# # Error Handling
+# ---------------------------------------------------
+
+# nums = [5, 2, 3, 10]
+
+# try:
+#     avg = sum(nums) / len(nums)
+#     print('The average of the list is: ', avg)
+
+# except:
+#     print('Cannot compute average - make sure you enter a list of integers!')
+
+# finally:
+#     print('Feel free to rerun the code with another list of integers!')

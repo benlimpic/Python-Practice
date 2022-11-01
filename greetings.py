ccdb790bb6c512e5a0726d08d7d14a00d7d15b31
init.defaultBranch <benlimpic@gmail.com>
2022-10-31 23:46:40 -0700

    initial commit

---
+++ greetings.py
@@ -0,0 +1,9 @@
+def main():
+    print('This line is from the main')
+    secondary_function()
+
+def secondary_function():
+    print('This is from the secondary function')
+
+if __name__ == '__main__':
+    main()
